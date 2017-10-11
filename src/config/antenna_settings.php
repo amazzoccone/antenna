97a383edd44d7e4733160d17cdf980c202ae48a4
Nicolás Sinisterra <nsinisterra@bondacom.com>
2017-10-11 13:50:37 -0300

    Add config template

---
+++ src/config/antenna_settings.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | User's credentials
+    |--------------------------------------------------------------------------
+    |
+    | Used to manage apps
+    | You can found it on https://onesignal.com/users/me
+    */
+    'userKey' => env('ONESIGNAL_USER_KEY'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default APP
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default APP that should be used
+    | by the framework. Just store away!
+    |
+    | If you have just one app, then, we will get your app id and app key from
+    | .env file
+    |
+    */
+    'default_app' => 'default',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Apps
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure as many apps as you wish
+    |
+    */
+    'apps' => [
+        'default' => [
+            'id' => env('ONESIGNAL_APP_ID'),
+            'key' => env('ONESIGNAL_APP_KEY'),
+        ]
+    ]
+
+];
