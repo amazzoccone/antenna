e7c3c62276cb826a71d0b9f6ee358475b605da8e
Andres Mazzo <amazzoccone@bondacom.com>
2017-12-21 16:12:44 -0300

    Fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Bondacom\antenna\Tests;
 
+use Bondacom\antenna\AntennaRequester;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -9,6 +10,8 @@ abstract class TestCase extends BaseTestCase
     public function setUp()
     {
         parent::setUp();
+
+        $this->mockRequester();
     }
 
     /**
@@ -32,5 +35,24 @@ abstract class TestCase extends BaseTestCase
         $mock = $arguments ? \Mockery::mock($class, $arguments) : \Mockery::mock($class);
         $this->app->instance($class, $mock);
         return $mock;
+    }
+
+    /**
+     * @return $this
+     */
+    private function mockRequester()
+    {
+        $obj = new \stdClass();
+        $obj->id = random_int(1, 9999);
+        $obj->name = 'Testing';
+        $obj->basic_auth_key = str_random();
+
+        $mock = $this->mock(AntennaRequester::class)->makePartial();
+        $mock->shouldReceive('get')->andReturn($obj);
+        $mock->shouldReceive('put')->andReturn($obj);
+        $mock->shouldReceive('post')->andReturn($obj);
+        $mock->shouldReceive('setUserKey')->andReturnSelf();
+
+        return $this;
     }
 }
