b64fadb05ee81a0dc1f7a9269c84593cd7021c51
Andres Mazzo <amazzoccone@bondacom.com>
2018-01-25 12:10:42 -0300

    Fix fillable property of Notification model

--- src/Models/Notification.php
+++ src/Models/Notification.php
@@ -7,13 +7,8 @@ use Bondacom\Antenna\Utilities\Model;
 
 class Notification extends Model
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'id',
+    /*
+     * Attributes when notification is created
         'successful',
         'failed',
         'converted',
@@ -25,6 +20,81 @@ class Notification extends Model
         'canceled',
         'headings',
         'contents',
+     * */
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'included_segments',
+        'excluded_segments',
+        'filters',
+        'include_player_ids',
+        'app_id',
+        //Content & Language
+        'contents',
+        'headings',
+        'subtitle',
+        'template_id',
+        'content_available',
+        'mutable_content',
+        //Attachments
+        'data',
+        'url',
+        'ios_attachments',
+        'big_picture',
+        'adm_big_picture',
+        'chrome_big_picture',
+        //Action Buttons
+        'buttons',
+        'web_buttons',
+        'ios_category',
+        //Appearance
+        'android_channel_id',
+        'existing_android_channel_id',
+        'android_background_layout',
+        'small_icon',
+        'large_icon',
+        'adm_small_icon',
+        'adm_large_icon',
+        'chrome_web_icon',
+        'chrome_web_image',
+        'chrome_icon',
+        'ios_sound',
+        'android_sound',
+        'adm_sound',
+        'wp_sound',
+        'wp_wns_sound',
+        'android_led_color',
+        'android_accent_color',
+        'android_visibility',
+        'ios_badgeType',
+        'ios_badgeCount',
+        'collapse_id',
+        //Delivery
+        'send_after',
+        'delayed_option',
+        'delivery_time_of_day',
+        'ttl',
+        'priority',
+        //Grouping & Collapsing
+        'android_group',
+        'android_group_message',
+        'adm_group',
+        'adm_group_message',
+        //Platform to Deliver To
+        'isIos',
+        'isAndroid',
+        'isAnyWeb',
+        'isChromeWeb',
+        'isFirefox',
+        'isSafari',
+        'isWP',
+        'isWP_WNS',
+        'isAdm',
+        'isChrome',
     ];
 
     /**
